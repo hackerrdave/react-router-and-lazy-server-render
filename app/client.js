5f1845fe24b20a4df2c9d1c9f2030582fe23bbb2
Dave Kerr <davek09@gmail.com>
2016-01-30 18:09:34 -0500

    set up client-side app script

---
+++ app/client.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { match, Router } from 'react-router';
+import { render } from 'react-dom';
+import { createHistory } from 'history';
+import routes from './routes/RootRoute';
+
+const { pathname, search, hash } = window.location;
+const location = `${pathname}${search}${hash}`;
+
+match({ routes, location }, () => {
+  render(
+    <Router routes={routes} history={createHistory()}/>,
+    document.getElementById('app')
+  );
+});
+
